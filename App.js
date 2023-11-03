815ea5498e1b37abf963023e1b82e4671ea127df
Naveen M <vyshnavipopuri13@yahoo.com>
2023-11-03 08:07:15 +0530

    Adding App.js

--- App.js
+++ App.js
@@ -2,3 +2,5 @@ let header= React.createElement("h1",{},"Hello World");
 console.log(header)
 let root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
+
+console.log("Vyshnavi_commit");
